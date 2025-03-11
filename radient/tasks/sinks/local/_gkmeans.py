8f1d1b5610d365c6a396e160f79c96bd792af35a
Frank Liu <frank@frankzliu.com>
2025-03-11 10:56:58 -0700

    Improve GKMeans performance

--- radient/tasks/sinks/local/_gkmeans.py
+++ radient/tasks/sinks/local/_gkmeans.py
@@ -1,4 +1,4 @@
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 import numpy as np
 
@@ -16,28 +16,45 @@ else:
     DataLoader = LazyImport("torch.utils.data", attribute="DataLoader")
     TensorDataset = LazyImport("torch.utils.data", attribute="TensorDataset")
 
-WARMUP_EPOCHS = 10
+
+WARMUP_EPOCHS = 5
 
 
-def calculate_distances(
+def _euclidean_distance(
+    A: torch.Tensor,
+    B: torch.Tensor
+):
+    A_norm = (A**2).sum(dim=1, keepdim=True)
+    B_norm = (B**2).sum(dim=1, keepdim=True).T
+    dist = A_norm + B_norm - 2.0 * A @ B.T
+    return dist.clamp(min=0.0).sqrt()
+
+
+def _lp_norm_distance(
     A: torch.Tensor,
     B: torch.Tensor,
-    kind: str = "Lp-norm",
-    p: float = 2.0
+    p: float = 2
 ):
-    if kind == "Lp-norm":
-        return torch.cdist(A.unsqueeze(0), B.unsqueeze(0), p=p).squeeze(dim=0)
-    else:
-        raise NotImplementedError(f"Distance metric {kind} not implemented.")
+    return torch.cdist(A.unsqueeze(0), B.unsqueeze(0), p=p).squeeze(0)
 
 
-class KMeans(nn.Module):
+def _cosine_distance(
+    A: torch.Tensor,
+    B: torch.Tensor
+):
+    A_norm = (A**2).sum(dim=1, keepdim=True).sqrt()
+    B_norm = (B**2).sum(dim=1, keepdim=True).sqrt().T
+    return 1.0 - (A @ B.T) / (A_norm * B_norm)
+
+
+class GKMeans(nn.Module):
     def __init__(
         self,
         n_clusters: int = 8,
-        max_iter: int = 600,
-        tol: float = 1e-4,
-        random_state: Optional[int] = None,
+        max_iter: int = 50,
+        tol: float = 1e-3,
+        random_state: int | None = None,
+        distance_metric: str = "lp-norm",
         learning_rate: float = 1e-4,
         batch_size: int = 4096,
         size_decay: float = 4.0,
@@ -48,10 +65,21 @@ class KMeans(nn.Module):
         self._n_clusters = n_clusters
         self._max_iter = max_iter
         self._tol = tol
+
         self._learning_rate = learning_rate
         self._batch_size = batch_size
         self._size_decay = size_decay
         self._verbose = verbose
+
+        # Set distance metric
+        if distance_metric == "euclidean":
+            self._calculate_distances = _euclidean_distance
+        elif distance_metric == "cosine":
+            self._calculate_distances = _cosine_distance
+        elif distance_metric == "lp-norm":
+            self._calculate_distances = _lp_norm_distance
+        else:
+            raise ValueError(f"Invalid distance metric: {distance_metric}")
 
         # Set seed
         if random_state:
@@ -60,17 +88,18 @@ class KMeans(nn.Module):
 
     @property
     def cluster_centers_(self):
-        return self.C.detach().numpy()
+        return self._C.detach().numpy()
 
     def _init_centers(self, X: torch.Tensor):
         # K-means++ initialization
-        C_init = torch.empty((self._n_clusters, X.shape[1]), dtype=torch.float32)
+        shape = (self._n_clusters, X.shape[1])
+        C_init = torch.empty(shape, dtype=torch.float32)
         C_init[0,:] = X[np.random.choice(X.shape[0])]
         for n in range(1, self._n_clusters):
-            d, _ = calculate_distances(X, C_init[:n]).min(dim=1)
+            d, _ = self._calculate_distances(X, C_init[:n,:]).min(dim=1)
             p = (d**2 / d.pow(2).sum()).numpy()
             C_init[n,:] = X[np.random.choice(X.shape[0], p=p)]
-        self.C = nn.Parameter(C_init)
+        self._C = nn.Parameter(C_init)
 
     def _lr_lambda(self, epoch: int):
         if epoch < WARMUP_EPOCHS:
@@ -82,7 +111,7 @@ class KMeans(nn.Module):
             return 0.5 * (1 + np.cos(np.pi * (epoch - WARMUP_EPOCHS) / decay_epochs))
 
     def forward(self, X: torch.Tensor):
-        return calculate_distances(X, self.C)
+        return self._calculate_distances(X, self._C)
 
     def forward_loss(self, X: torch.Tensor):
         d = self.forward(X) ** 2
@@ -93,11 +122,10 @@ class KMeans(nn.Module):
     def fit(
         self,
         X: np.ndarray,
-        y: Optional[np.array] = None,
-        sample_weight: Optional[np.array] = None
+        y: np.ndarray | None = None,
+        sample_weight: np.ndarray | None = None
     ):
-        
-        X = torch.tensor(X, dtype=torch.float32)
+        X = torch.from_numpy(X)
         self._init_centers(X)
 
         # Create dataloader, optimizer, and scheduler
@@ -106,31 +134,34 @@ class KMeans(nn.Module):
             batch_size=self._batch_size,
             shuffle=True
         )
+
         optimizer = optim.SGD(self.parameters(), lr=self._learning_rate, momentum=0.9)
         scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=self._lr_lambda)
 
         # Training loop
         for epoch in range(self._max_iter):
-            for n, (batch,) in enumerate(dataloader):
+            for batch, in dataloader:
                 optimizer.zero_grad()
                 loss = self.forward_loss(batch)
                 loss.backward()
                 optimizer.step()
                 #print(f"    Train loss: {loss.item():.3f}")
-            loss = self.forward_loss(X)
             scheduler.step()
             if self._verbose:
-                print(f"Epoch {epoch+1}, loss: {loss.item():.5f}")
+                with torch.no_grad():
+                    loss = self.forward_loss(X)
+                    print(f"Epoch {epoch+1}, loss: {loss.item():.5f}")
 
-    def predict(self, X: np.array):
-        X = torch.tensor(X, dtype=torch.float32)
-        return self.forward(X).argmin(dim=1).numpy()
+    def predict(self, X: np.ndarray):
+        X = torch.from_numpy(X)
+        a = self.forward(X).argmin(dim=1)
+        return a.numpy()
 
     def fit_predict(
         self,
         X: np.ndarray,
-        y: Optional[np.array] = None,
-        sample_weight: Optional[np.array] = None
+        y: np.ndarray | None = None,
+        sample_weight: np.ndarray | None = None
     ):
         self.fit(X, y=y, sample_weight=sample_weight)
         return self.predict(X)
--- radient/tasks/sinks/local/gann.py
+++ radient/tasks/sinks/local/gann.py
@@ -1,23 +1,29 @@
+from multiprocessing import Pool
+import time
+
 import numpy as np
 
-from radient.tasks.sinks.local._gkmeans import KMeans
+from radient.tasks.sinks.local._gkmeans import GKMeans
 
 
-class GANN():
+MAX_LEAF_SIZE = 200
+
+
+class GANNNode():
 
     def __init__(
         self,
-        centroid: np.ndarray | None = None,
+        dataset: np.ndarray,
         indexes: np.ndarray | None = None,
-        dataset: np.ndarray | None = None,
-        sealed: bool = False,
+        centroid: np.ndarray | None = None,
+        verbose: bool = False,
         **kwargs
     ):
         super().__init__()
+        self._dataset = dataset
+        self._indexes = indexes
         self._centroid = centroid
-        self._indexes = indexes if indexes is not None else []
-        self._dataset = dataset if dataset is not None else []
-        self._sealed = sealed
+        self._verbose = verbose
         self._children = None
     
     @property
@@ -29,90 +35,128 @@ class GANN():
         return self._children
 
     @property
-    def sealed(self):
-        return self._sealed
-
-    @property
     def vectors(self):
-        if self._sealed:
-            return self._dataset[self._indexes]
-        raise ValueError("Index must be sealed.")
+        if self._indexes is None:
+            return self._dataset
+        return self._dataset[self._indexes]
 
-    def insert(self, vector: np.ndarray):
-        """Inserts a vector into an unsealed index.
+    def split(self, recursive: bool = True, spill: float = 0.0):
+        """Splits the index.
         """
-        if self._sealed:
-            raise ValueError("Cannot insert into a sealed index.")
-        self._dataset.append(vector)
-        self._indexes.append(len(self._dataset) - 1)
-
-    def seal(self):
-        """Seals the index.
-        """
-        if self._sealed:
-            raise ValueError("Index is already sealed.")
-        self._sealed = True
-        self._dataset = np.array(self._dataset)
-        self._indexes = np.array(self._indexes)
-
-    def build(self):
-        """Builds the index.
-        """
-        if not self._sealed:
-            raise ValueError("Index must be sealed.")
-        if self._indexes.size < 500:
+        if self._indexes is not None and len(self._indexes) <= MAX_LEAF_SIZE:
+            self._indexes = set(self._indexes)
             return
 
         vectors = self.vectors
-        self._sealed = True
-        print("Building index with", len(vectors), "vectors.")
+        #print("Splitting index with", len(vectors), "vectors.")
 
         # Get indexes for each cluster
-        kmeans = KMeans(n_clusters=2)
-        a = kmeans.fit_predict(vectors)
+        gkmeans = GKMeans(n_clusters=2, verbose=self._verbose)
+        a = gkmeans.fit_predict(vectors)
+        C = list(gkmeans.cluster_centers_)
         idxs_C = [np.where(a==n)[0] for n in range(2)]
 
-        # Determine the hyperplane that separates the two clusters
-        # (i.e. the two centroids)
-        C = list(kmeans.cluster_centers_)
-        w = C[1] - C[0]
-        b = -(C[1] + C[0]).dot(w) / 2.0
+        if spill > 0:
+            # Determine the hyperplane that separates the two clusters
+            # (i.e. the two centroids)
+            w = C[1] - C[0]
+            b = -(C[1] + C[0]).dot(w) / 2.0
 
-        # Compute each point's distance to the hyperplane
-        d = (vectors.dot(w) + b) / np.linalg.norm(w)
+            # Compute each point's distance to the hyperplane
+            d = (vectors.dot(w) + b) / np.linalg.norm(w)
 
-        # For each of the two clusters, add 1/3 of the points that are in the
-        # other cluster but are very close to the separating hyperplane
-        n_add = int(len(vectors) / 6.0)
-        idxs_C_add = [
-            np.where(d > 0, d, np.inf).argsort()[:n_add],
-            np.where(d <= 0, d, -np.inf).argsort()[-n_add:]
-        ]
-        idxs_C = [np.concatenate([idxs_C[n], idxs_C_add[n]]) for n in range(2)]
+            # For each of the two clusters, add `spill` of the points that are
+            # in the other cluster but are very close to the separating
+            # hyperplane
+            n_add = int(len(vectors) * spill / 2.0)
+            idxs_C_add = [
+                np.where(d > 0, d, np.inf).argsort()[:n_add],
+                np.where(d <= 0, d, -np.inf).argsort()[-n_add:]
+            ]
+            idxs_C = [np.concatenate([idxs_C[n], idxs_C_add[n]]) for n in range(2)]
+
+        if self._indexes is not None:
+            idxs_C = [self._indexes[idxs_C[n]] for n in range(2)]
 
         # Create child nodes
-        del vectors
         self._children = [
-            GANN(
-                centroid=C[n],
-                indexes=self._indexes[idxs_C[n]],
+            GANNNode(
                 dataset=self._dataset,
-                sealed=True
-            ) for n in range(2)]
-        for child in self._children:
-            child.build()
+                indexes=idxs_C[n],
+                centroid=C[n],
+                verbose=self._verbose
+            ) for n in range(2)
+        ]
 
-    def search(self, vector: np.ndarray, top_k: int = 10):
+        # Build indexes for each child node
+        if recursive:
+            for child in self._children:
+                child.split(recursive=recursive, spill=spill)
+
+    def get_candidates(self, vector: np.ndarray, top_k: int = 10):
         """Searches for the nearest vector in the index.
         """
-        if not self._sealed:
-            raise ValueError("Build the index before searching.")
-        
         if self._children is None:
-            vectors = self.vectors
-            d = np.linalg.norm(vectors - vector, axis=1)
-            return self._indexes[np.argsort(d)[:top_k]]
+            return self._indexes
     
         # Determine which child node to search
         d = [np.linalg.norm(node.centroid - vector) for node in self._children]
-        return self._children[np.argmin(d)].search(vector, top_k=top_k)
+        return self._children[np.argmin(d)].get_candidates(vector, top_k=top_k)
+
+
+class GANN():
+
+    def __init__(
+        self,
+        n_trees: int = 4,
+        spill: float = 0.0,
+        verbose: bool = False,
+        **kwargs
+    ):
+        super().__init__()
+        self._n_trees = n_trees
+        self._spill = spill
+        self._verbose = verbose
+        self._dataset = []
+
+    def _build_tree(self, n: int) -> GANNNode:
+        np.random.seed(None)
+        node = GANNNode(dataset=self._dataset, verbose=self._verbose)
+        node.split(recursive=True, spill=self._spill)
+        return node
+
+    @property
+    def n_trees(self):
+        return self._n_trees
+
+    @property
+    def sealed(self):
+        return hasattr(self, "_trees")
+
+    def insert(self, vector: np.ndarray):
+        """Inserts a vector into the index.
+        """
+        if self.sealed:
+            raise ValueError("Cannot insert into a sealed index.")
+        self._dataset.append(vector)
+
+    def build(self, n_proc: int = 1):
+        if self.sealed:
+            raise ValueError("Index is already built.")
+
+        self._dataset = np.array(self._dataset, dtype=np.float32)
+
+        self._trees = []
+        with Pool(n_proc) as pool:
+            self._trees = pool.map(self._build_tree, range(self._n_trees))
+    
+    def search(self, vector: np.ndarray, top_k: int = 10) -> set[int]:
+        if not self.sealed:
+            raise ValueError("Build the index before searching.")
+
+        candidates = set()
+        for tree in self._trees:
+            candidates.update(tree.get_candidates(vector, top_k=top_k))
+        candidates_ = np.array(list(candidates))
+        distances = np.linalg.norm(self._dataset[candidates_] - vector, axis=1)
+        return candidates_[distances.argsort()[:top_k]]
